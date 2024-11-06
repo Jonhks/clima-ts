e956721639298aa279342cb6181c224e39cdeed6
Jonhks <jonhgx@gmail.com>
2024-11-06 15:37:08 -0600

    update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Buscador de clima</h1>
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
